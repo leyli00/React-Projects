617785fbcf74faee0466cc5c13004c9ff4896c19
leyli00 <leylidostluk@gmail.com>
2024-12-21 19:02:03 +0300

    E-Commrce project header completed

---
+++ 2.E-COMMERCE/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { Container } from '@mui/material'
+import Header from './Components/Header'
+
+function App() {
+
+  return (
+   <div>
+    <Container>
+      <Header/>
+    </Container>
+   </div>
+  )
+}
+
+export default App
